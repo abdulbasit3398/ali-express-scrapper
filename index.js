35711152c7171a70fcaaa43473e06f64a40cb310
Abdul Basit <basit.dzinemedia@gmail.com>
2020-08-24 01:06:27 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+  limit: '50mb'
+}));
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader("Access-Control-Allow-Headers", 'Authorization,Origin,X-Requested-With,Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+const scrape = require('aliexpress-product-scraper');
+app.get('/', (req, res) => {
+    res.send('working');
+})
+
+app.post('/',(req,res) =>{
+  console.log(req.body.product_id)
+  const product = scrape(req.body.product_id);
+
+  product.then(result => {
+    res.json(result);
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
